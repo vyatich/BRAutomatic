ca61b34ec251b7a113019a1da3c28f8dc6fd3c95
viktor <vik_ivy@mail.ru>
2018-11-09 11:02:07 +0400

    Create BCryptPassword encoder

--- src/main/java/com/brautomatic/configuration/SecurityConfiguration.java
+++ src/main/java/com/brautomatic/configuration/SecurityConfiguration.java
@@ -2,12 +2,15 @@ package com.brautomatic.configuration;
 
 import com.brautomatic.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +18,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserServiceImpl userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(6);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,6 +49,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .userDetailsService(userService)
-            .passwordEncoder(NoOpPasswordEncoder.getInstance()); //todo change encoder
+            .passwordEncoder(passwordEncoder);
     }
 }
--- src/main/java/com/brautomatic/service/UserServiceImpl.java
+++ src/main/java/com/brautomatic/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.brautomatic.service;
 
+import com.brautomatic.model.Role;
 import com.brautomatic.model.User;
 import com.brautomatic.repository.UserRepository;
 import com.brautomatic.service.interfaces.UserService;
@@ -7,16 +8,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void addUser(User user) {
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
